452ec5887224b9f3fcba2fd66c4888b71e9cdfe6
Muhammad Yudi Setiawan <yudsetiawann@gmail.com>
2025-11-22 10:24:52 +0700

    edit text salesStatsOverview

--- app/Filament/Widgets/SalesStatsOverview.php
+++ app/Filament/Widgets/SalesStatsOverview.php
@@ -24,7 +24,7 @@ class SalesStatsOverview extends BaseWidget
 
         return [
             Stat::make('Total Pendapatan', Number::currency($totalRevenue, 'IDR'))
-                ->description('Pemasukan bersih')
+                ->description('Total Income')
                 ->descriptionIcon('heroicon-m-arrow-trending-up')
                 ->chart([70000, 150000, 120000, 200000, $totalRevenue]) // Dummy chart tren pendapatan
                 ->color('success'), // Hijau
@@ -36,7 +36,7 @@ class SalesStatsOverview extends BaseWidget
                 ->color('primary'), // Amber/Sesuai tema
 
             Stat::make('Total Atlet', $totalParticipants . ' Orang')
-                ->description('Peserta terdaftar di database')
+                ->description('Atlet terdaftar di database')
                 ->descriptionIcon('heroicon-m-users')
                 ->chart([100, 120, 140, 150, $totalParticipants]) // Dummy chart pertumbuhan user
                 ->color('info'), // Biru
