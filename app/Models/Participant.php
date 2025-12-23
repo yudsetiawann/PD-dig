dd9c2179d0c53be7e85320c5a2667b5ed4c503d0
Muhammad Yudi Setiawan <yudsetiawann@gmail.com>
2025-12-23 22:54:50 +0700

    add relation to user

--- app/Models/Participant.php
+++ app/Models/Participant.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Participant extends Model
 {
@@ -19,5 +20,10 @@ class Participant extends Model
     public function attendances()
     {
         return $this->hasMany(TrainingAttendance::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
