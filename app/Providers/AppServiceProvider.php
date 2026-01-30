eec7e78730bf0df838291d9b23c7bf70ac664999
Muhammad Yudi Setiawan <yudsetiawann@gmail.com>
2026-01-30 07:44:43 +0700

    update: add custom verify email

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,8 @@ use App\Observers\UserObserver;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,5 +27,15 @@ class AppServiceProvider extends ServiceProvider
     {
         Model::preventLazyLoading();
         User::observe(UserObserver::class);
+
+        // Kustomisasi Bahasa Email Verifikasi
+        VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
+            return (new MailMessage)
+                ->subject('Verifikasi Alamat Email Anda') // Judul Email
+                ->greeting('Halo!') // Sapaan pembuka
+                ->line('Silakan klik tombol di bawah ini untuk memverifikasi alamat email Anda.') // Kalimat pengantar
+                ->action('Verifikasi Email Saya', $url) // Tulisan di Tombol
+                ->line('Jika Anda tidak merasa mendaftar akun ini, abaikan saja pesan ini.'); // Kalimat penutup
+        });
     }
 }
